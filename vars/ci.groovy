46b8aa4141cec97d250c04ffe5811ab9fa220628
TARAK <tarakaramtirumala@gmail.com>
2023-04-11 15:44:33 +0530

    changes

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,24 +1,33 @@
-def call () {
-    pipeline {
-        agent any
+def call() {
+  if (!env.sonar_extra_opts) {
+    env.sonar_extra_opts=""
+  }
+  pipeline {
+    agent any
 
-        stages {
+    stages {
 
-            stage('Compile/Build') {
-                steps {
-                    script {
-                        common.compile()
-                    }
-                }
-            }
-
-            stage('Test Cases') {
-                steps {
-                    script {
-                        common.testcases()
-                    }
-                }
-            }
+      stage('Compile/Build') {
+        steps {
+          script {
+            common.compile()
+          }
         }
+      }
+
+      stage('Test Cases') {
+        steps {
+          script {
+            common.testcases()
+          }
+        }
+      }
+
+      stage('Code Quality') {
+        steps {
+          script {
+            common.codequality()
+          }
+        }
+      }
     }
-}
