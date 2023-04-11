551ef0cb1afab7ce8dd15ee517692b87a13e1d64
TARAK <tarakaramtirumala@gmail.com>
2023-04-11 08:02:42 +0530

    changes

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -15,7 +15,7 @@ def call() {
 
             stage('Init') {
                 steps {
-                    sh 'terraform init -backend-config=env-${ENV}/state.tfvars'
+                    sh 'terraform init -backend-config=env-dev/state.tfvars'
                 }
             }
 
