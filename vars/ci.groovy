47a637ca488c0e9f122924dce91bc6eef429c8f4
TARAK <tarakaramtirumala@gmail.com>
2023-04-13 08:12:12 +0530

    changes

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,40 +1,42 @@
-def call() {
-  if (!env.sonar_extra_opts) {
-    env.sonar_extra_opts=""
-  }
-  pipeline {
-    agent any
 
-    stages {
-      stage('Compile/Build') {
-        steps {
-          script {
-            common.compile()
-          }
-        }
-      }
+def call () {
+    if (!env.sonar_extra_opts) {
+        env.sonar_extra_opts=""
+    }
+    pipeline {
+        agent any
 
-      stage('Test Cases') {
-        steps {
-          script {
-            common.testcases()
-          }
-        }
-      }
+        stages {
 
-      stage('Code Quality') {
-        steps {
-          script {
-            common.codequality()
-          }
+            stage('Compile/Build') {
+                steps {
+                    script {
+                        common.compile()
+                    }
+                }
+            }
+
+            stage('Test Cases') {
+                steps {
+                    script {
+                        common.testcases()
+                    }
+                }
+            }
+            stage('Code Quality') {
+                steps {
+                    script {
+                        common.codequality()
+                    }
+                }
+            }
         }
-      }
+        post {
+            failure {
+                mail body: "${component} - Pipeline Failed \n ${BUILD_URL}", from: 'nani.adamgilchrist@gmail.com', subject: "${component} - Pipeline Failed", to: 'nani.adamgiclhrist@gmail.com'
+
+            }
+        }
     }
 
-    post {
-      failure {
-        mail bcc:mail body: "<h1>${component} - Pipeline Failed \n ${BUILD_URL}</h1>", from: 'nani.adamgilchrist@gmail.com', subject: "${component} - Pipeline Failed", to: 'nani.adamgilchrist@gmail.com',  mimeType: 'text/html'
-      }
-    }
-  }
 }
