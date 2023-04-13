4f1b95de973bb921bfad3186053b8bfb775495f1
TARAK <tarakaramtirumala@gmail.com>
2023-04-13 15:44:50 +0530

    chnages

--- vars/common.groovy
+++ vars/common.groovy
@@ -18,5 +18,8 @@ def testcases() {
 }
 
 def codequality() {
+     withAWSParameterStore(credentialsId: 'PARAM1', naming: 'absolute', path: '/sonarqube', recursive: true, regionName: 'us-east-1') {
     sh 'sonar-scanner -Dsonar.host.url=http://172.31.4.104:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectKey=${component} ${sonar_extra_opts}'
+ }
+
 }
