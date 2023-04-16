6459cf6d8b56e46ebd79ffc304ee1681f6e058b9
TARAK <tarakaramtirumala@gmail.com>
2023-04-16 22:56:14 +0530

    changes

--- vars/ci-scripted.groovy
+++ vars/ci-scripted.groovy
@@ -5,14 +5,33 @@ def call () {
     }
     node('workstation') {
 
+      try {
+
+        stage('check out code') {
+           sh 'ls -l'
+           cleanws()
+           sh 'ls -l'
+           git branch: 'main', url: 'http;//github.com/taraka1996/cart'
+           sh 'ls -l'
+        }      
+
         stage('compile/build') {
+            sh 'env'
             common.compile()
 
-        
-      }
+        }
+      
 
         stage('Test Cases') {
            common.testcases()
-      }
-                }
+        }
+
+        stage('code quality') {
+            coomon.codequality()
+        }
+
+      } catch (e) {
+           mail body: "<h1>${component} - Pipeline Failed \n ${BUILD_URL}</h1>", from: 'tarakatirumala@gmail.com', subject: "${component}- Pipeline Failed", to: 'tarakatirumala@gmail.com',  mimeType: 'text/html'
+        }
+                
             
