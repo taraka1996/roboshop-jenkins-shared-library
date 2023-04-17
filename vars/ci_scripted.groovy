914ba4a28654cb1c89d05046a9f0bfe9a662878e
TARAK <tarakaramtirumala@gmail.com>
2023-04-17 08:00:55 +0530

    changes

--- vars/ci_scripted.groovy
+++ vars/ci_scripted.groovy
@@ -1,37 +1,30 @@
-
-def call () {
+def call() {
     if (!env.sonar_extra_opts) {
         env.sonar_extra_opts=""
     }
     node('workstation') {
-
-      try {
-
-        stage('check out code') {
-           sh 'ls -l'
-           cleanws()
-           sh 'ls -l'
-           git branch: 'main', url: 'http;//github.com/taraka1996/cart'
-           sh 'ls -l'
-        }      
-
-        stage('compile/build') {
+        try {
+            stage('check out code') {
+                sh 'ls -l'
+                cleanWs()
+                sh 'ls -l'
+                git branch: 'main', url: 'http://github.com/taraka1996/cart'
+                sh 'ls -l'
+            }  
             sh 'env'
-            common.compile()
-
+            if (env.BRANCH_NAME != "main") {
+                stage('compile/build') {            
+                    common.compile()
+                }        
+            }
+            stage('Test Cases') {
+                common.testcases()
+            }
+            stage('code quality') {
+                common.codequality()
+            }
+        } catch (e) {
+            mail body: "<h1>${component} - Pipeline Failed \n ${BUILD_URL}</h1>", from: 'tarakaramtirumala@gmail.com', subject: "${component}- Pipeline Failed", to: 'tarakaramtirumala@gmail.com',  mimeType: 'text/html'
         }
-      
-
-        stage('Test Cases') {
-           common.testcases()
-        }
-
-        stage('code quality') {
-            coomon.codequality()
-        }
-
-      } catch (e) {
-           mail body: "<h1>${component} - Pipeline Failed \n ${BUILD_URL}</h1>", from: 'tarakatirumala@gmail.com', subject: "${component}- Pipeline Failed", to: 'tarakatirumala@gmail.com',  mimeType: 'text/html'
-        }
-                
-            
+    }
+}
