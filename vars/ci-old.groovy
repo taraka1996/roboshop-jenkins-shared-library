abb80acfa07aa68ee5a6cd7d438e83a780fb37c9
TARAK <tarakaramtirumala@gmail.com>
2023-04-19 11:45:38 +0530

    changes

--- vars/ci-old.groovy
+++ vars/ci-old.groovy
@@ -1,54 +1,42 @@
-def call() {
+
+def call () {
     if (!env.sonar_extra_opts) {
         env.sonar_extra_opts=""
     }
-    if(env.TAG_NAME !=~ ".*") {
-        env.GTAG = "true"
-    } else {
-        env.GTAG = 'false'
-    }
+    pipeline {
+        agent any
 
-    node('workstation') {
-        try {
-            stage('check out code') {                      
-                cleanWs()                                
-                git branch: 'main', url: 'http://github.com/taraka1996/cart'
-                             
-            } 
+        stages {
 
-            sh 'env' 
-
-            if (env.BRANCH_NAME != "main" ) {
-                stage('compile/build') {            
-                    common.compile()
-                }        
-            } 
-
-            println GTAG
-            println BRANCH_NAME      
-
-            if(env.GTAG != "true" && env.BRANCH_NAME != "main" ) {
-                stage('Test Cases') {
-                    common.testcases()
+            stage('Compile/Build') {
+                steps {
+                    script {
+                        common.compile()
+                    }
                 }
             }
 
-            if (BRANCH_NAME ==~ "PR-.*") {              
-                stage('code quality') {
-                    common.codequality()
-                }
-                
-                if(env.GTAG != "true"  ) {
-                    stage('package') {
-                        common.prepareArtifacts()
-                    }
-                    stage('Artifact upload') {
+            stage('Test Cases') {
+                steps {
+                    script {
                         common.testcases()
                     }
                 }
             }
-        } catch (e) {
-            mail body: "<h1>${component} - Pipeline Failed \n ${BUILD_URL}</h1>", from: 'tarakaramtirumala@gmail.com', subject: "${component}- Pipeline Failed", to: 'tarakaramtirumala@gmail.com',  mimeType: 'text/html'
+            stage('Code Quality') {
+                steps {
+                    script {
+                        common.codequality()
+                    }
+                }
+            }
+        }
+        post {
+            failure {
+                mail body: "${component} - Pipeline Failed \n ${BUILD_URL}", from: 'nani.adamgilchrist@gmail.com', subject: "${component} - Pipeline Failed", to: 'nani.adamgiclhrist@gmail.com', mimeType: 'text/html'
+
+            }
         }
     }
+
 }
