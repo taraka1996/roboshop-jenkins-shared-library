f40ad4e18e17626312416844195b79dbeab97894
TARAK <tarakaramtirumala@gmail.com>
2023-04-21 11:21:05 +0530

    changes

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -14,7 +14,7 @@ def call() {
 
       stage('Check Out Code') {
         cleanWs()
-        git branch: 'main', url: "https://github.com/raghudevopsb71/${component}"
+        git branch: 'main', url: "https://github.com/taraka1996/${component}"
       }
 
       sh 'env'
