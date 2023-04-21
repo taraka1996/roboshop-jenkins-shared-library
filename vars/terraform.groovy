8916ac5f9576f97d4e13cefa4319b9d6d9f71acc
TARAK <tarakaramtirumala@gmail.com>
2023-04-21 11:03:41 +0530

    changes

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -21,7 +21,7 @@ def call() {
 
             stage('Apply') {
                 steps {
-                    sh 'terraform ${ACTION} -auto-approve -var-fileenv-=${ENV}/main.tfvars'
+                    sh 'terraform ${ACTION} -auto-approve -var-file=env-${ENV}/main.tfvars'
                     // sh 'echo'
                 }
             }
